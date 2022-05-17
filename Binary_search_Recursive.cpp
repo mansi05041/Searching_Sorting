e63abf2fa61a997f42187b5a33b155578269d1d0
Mansi joshi <77066770+mansi05041@users.noreply.github.com>
2022-05-17 19:31:36 +0530

    Update Binary_search_Recursive.cpp

--- Binary_search_Recursive.cpp
+++ Binary_search_Recursive.cpp
@@ -1,11 +1,5 @@
 // Binary_search_Recursive.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // Binary Search -Recursive
-/*Name: Mansi joshi
-* Roll no: 2019/102
-* Course: BSc Physical Science
-* Subject : Analysis of Algorithm
-*/ 
-
 /* Time Complexity :
 * Best case : O(1)
 * Worst case : O(logn)
